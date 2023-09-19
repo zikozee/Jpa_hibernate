3313f7e215ee499069c37a596bb428113da7ea96
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2023-09-19 02:55:51 +0100

    Using Class Configuration in place of xml closes #1

--- src/main/java/com/ziko/Main.java
+++ src/main/java/com/ziko/Main.java
@@ -1,22 +1,28 @@
 package com.ziko;
 
 import com.ziko.entities.Product;
+import com.ziko.persistence.CustomPersistenceUnitInfo2;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit"); // for xml
+        EntityManagerFactory emf = new HibernatePersistenceProvider()
+                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo2(), new HashMap<>());
         EntityManager em = emf.createEntityManager(); // represents the context
 
        try{
            em.getTransaction().begin();
 
            Product product = new Product();
-           product.setId(1);
-           product.setName("Beer");
+           product.setId(3);
+           product.setName("Books");
 
            em.persist(product);  // add this to context  --> NOT AN INSERT QUERY
 
